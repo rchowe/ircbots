2a8720fbf98a6d768c0b6a57388a22e2f7291c5f
RC Howe <rchowe1@gmail.com>
2010-06-23 23:33:48 -0400

    Carbon now supports random factoids.

--- carbon.rb
+++ carbon.rb
@@ -17,11 +17,24 @@ class Carbon < IRCBot
 				case m
 					when /^carbon[:,] (.+)/
 					# To carbon...
-						case $1.strip.downcase
-							when /^(.+?) [^\\](is|are) (.+)/
+						expr = $1.strip
+						case expr
+							when /^(.+?) (is|are) (.+)/
+								debug_puts "Storing #{$1} as #{expr}"
+								irc.send_msg_delay "OK, #{username}. #{$1} #{$2} #{$3}."
+								@memory[$1.downcase] = expr
+							when /^(.+?)<reply>(.+)$/
 								debug_puts "Storing #{$1} as #{$3}"
-								irc.send_msg_delay "OK, #{username}. #{$1} is #{$3}."
-								@memory[$1.downcase] = $3
+								irc.send_msg_delay "OK, #{username}. I will reply to #{$1} with #{$2}."
+								@memory[$1.downcase] = $2
+							when /^(say something random|random)$/
+								if @memory.size == 0
+									irc.send_msg_delay "I have nothing random to say"
+								else
+									a = @memory.to_a.sort_by{rand}.slice(0...1)[0][1].to_s
+									puts a
+									irc.send_msg_delay a
+								end
 							when /^hello$/
 								irc.send_msg_delay "Hello, #{username}!"
 						end
